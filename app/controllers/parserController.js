00df2d50e38fb8ee1cded9e1ea469676a131dd70
Eugene <eugene.shulga@dbbsoftware.com>
2018-09-30 20:01:18 +0300

    added parsing headers

--- app/controllers/parserController.js
+++ app/controllers/parserController.js
@@ -1,30 +1,37 @@
-const  request = require('request');
+const request = require('request');
 const cheerio = require('cheerio');
 const _ = require('lodash');
 const logger = requireLogger('parserController');
 
 const URL = 'https://dp.informator.ua/';
-
+const NO_ACCESS = 'Forbidden';
 module.exports = {
-	parseURL : function () {
+	parseURL: function () {
 		request(URL, function (err, res, body) {
-		if (err) throw  err;
-		let $ = cheerio.load(body);
+			if (err) throw  err;
+			let $ = cheerio.load(body);
 
-		const allLinksToNewsEntry =  _.map($('.infinite-post > a'),(a)=>(a.attribs['href']));
+			const allLinksToNewsEntry = _.map($('.infinite-post > a'), (a) => (a.attribs['href']));
 
-		if(allLinksToNewsEntry.length > 0) {
-			_.map(allLinksToNewsEntry, function (link) {
-				request(URL, function (err, res, body) {
-					if (err) throw  err;
-					$ = cheerio.load(body);
+			if (allLinksToNewsEntry.length > 0) {
+				_.map(allLinksToNewsEntry, function (link) {
+					request(link, function (err, res, body) {
+						if (err) throw  err;
+						if (!_.includes(body, NO_ACCESS)) {
+							$ = cheerio.load(body);
+						// parse header
+							let header = $('.post-title');
+							if (header.length > 0) {
+								header = header[0].children[0].data;
+							}
+						//parse text
+							
 
-					
-
+						}
+					})
 				})
-			})
 
-		}
+			}
 
 		});
 	}
